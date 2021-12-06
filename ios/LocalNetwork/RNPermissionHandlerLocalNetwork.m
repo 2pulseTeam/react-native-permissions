6959c751860f584f3a2d38e8e2e500922cd77193
Maxence <maxence.pinson@gmail.com>
2021-12-06 16:45:35 +0100

    test

--- ios/LocalNetwork/RNPermissionHandlerLocalNetwork.m
+++ ios/LocalNetwork/RNPermissionHandlerLocalNetwork.m
@@ -1,102 +1,93 @@
-#import "RNPermissionHandlerLocalNetwork.h"
-#include <ifaddrs.h>
-#include <sys/socket.h>
-#include <net/if.h>
-#include <netinet/in.h>
+#import "RNLocalNetworkPermission.h"
+#include <dns_sd.h>
 
-@import AVFoundation;
-
-@implementation RNPermissionHandlerLocalNetwork
-
-+ (NSArray<NSString *> * _Nonnull)usageDescriptionKeys {
-  return @[@"NSLocalNetWorkUsageDescription"];
+@implementation RNLocalNetworkPermission{
+    BOOL isBlocked;
+    RCTPromiseResolveBlock resolve;
+    NSTimer* timer;
+    DNSServiceRef browseRef;
+}
+RCT_EXPORT_MODULE();
++ (BOOL)requiresMainQueueSetup
+{
+  return YES;  // only do this if your module initialization relies on calling UIKit!
 }
 
-+ (NSString * _Nonnull)handlerUniqueId {
-  return @"ios.permission.LOCALNETWORK";
-}
-
-- (void)checkWithResolver:(void (^ _Nonnull)(RNPermissionStatus))resolve
-                 rejecter:(void (__unused ^ _Nonnull)(NSError * _Nonnull))reject {
-
-      static NSArray<NSData *> * addressesOfDiscardServiceOnBroadcastCapableInterfaces(void) {
-    struct ifaddrs * addrList = NULL;
-    int err = getifaddrs(&addrList);
-    if (err != 0) {
-        return @[];
-    }
-    NSMutableArray<NSData *> * result = [NSMutableArray array];
-    for (struct ifaddrs * cursor = addrList; cursor != NULL; cursor = cursor->ifa_next) {
-        if ( (cursor->ifa_flags & IFF_BROADCAST) &&
-             (cursor->ifa_addr != NULL)
-           ) {
-            switch (cursor->ifa_addr->sa_family) {
-            case AF_INET: {
-                struct sockaddr_in sin = *(struct sockaddr_in *) cursor->ifa_addr;
-                sin.sin_port = htons(9);
-                NSData * addr = [NSData dataWithBytes:&sin length:sizeof(sin)];
-                [result addObject:addr];
-            } break;
-            case AF_INET6: {
-                struct sockaddr_in6 sin6 = *(struct sockaddr_in6 *) cursor->ifa_addr;
-                sin6.sin6_port = htons(9);
-                NSData * addr = [NSData dataWithBytes:&sin6 length:sizeof(sin6)];
-                [result addObject:addr];
-            } break;
-            default: {
-                // do nothing
-            } break;
+RCT_EXPORT_METHOD(check:(double) timeOut
+                  withResolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+    if (@available(iOS 14, *)){
+        NSLog(@"check local network permission with timeout %f", timeOut);
+        if(self->timer!=nil){
+            [self->timer invalidate];
+            self->timer = nil;
+            reject(@"EDUP",@"please wait the check promise resolve",nil);
+            return;
+        }
+        if(self->browseRef!=nil){
+            DNSServiceRefDeallocate(self->browseRef);
+            self->browseRef = nil;
+        }
+        self->resolve = resolve;
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self->timer = [NSTimer scheduledTimerWithTimeInterval: timeOut
+                                                           target: self
+                                                         selector:@selector(onTimeOut:)
+                                                         userInfo: nil
+                                                          repeats:NO];
+            DNSServiceErrorType error = kDNSServiceErr_NoError;
+            error = DNSServiceBrowse(&(self->browseRef), 0, 0, @"_navicosettingsrestapi._tcp".UTF8String, NULL, browseCallback, (__bridge void *)(self));
+            if (error != kDNSServiceErr_NoError)
+            {
+                reject(@"EUNKOWN",@"can not start DNSServiceSetDispatchQueue",nil);
+                return;
             }
+            error = DNSServiceSetDispatchQueue(self->browseRef, dispatch_get_main_queue());
+            if (error != kDNSServiceErr_NoError)
+            {
+                reject(@"EUNKOWN",@"can not start DNSServiceSetDispatchQueue",nil);
+                return;
+            }
+        });
+    }else{
+        resolve([NSNumber numberWithBool:TRUE]);
+        return;
+    }
+}
+
+#pragma MARK - check
+-(void)resolvePromise:(BOOL) result {
+    if(self->resolve!=nil){
+        self->resolve([NSNumber numberWithBool:result]);
+        self->resolve = nil;
+    }
+}
+-(void)onTimeOut:(NSTimer *)timer {
+    DNSServiceRefDeallocate(self->browseRef);
+    self->browseRef = nil;
+    self->timer = nil;
+    [self resolvePromise:YES];
+}
+-(void)onResult {
+    if(self->timer){
+        [self->timer invalidate];
+        self->timer = nil;
+        DNSServiceRefDeallocate(self->browseRef);
+        self->browseRef = nil;
+        [self resolvePromise:NO];
+    }
+}
+
+static void browseCallback(DNSServiceRef sdRef, DNSServiceFlags flags, uint32_t interfaceIndex,
+                           DNSServiceErrorType errorCode, const char *serviceName,
+                           const char *regtype, const char *replyDomain, void *context) {
+    if (errorCode == kDNSServiceErr_PolicyDenied) {
+        NSLog(@"local network ungranted");
+        if(context!=nil){
+            RNLocalNetworkPermission* this = (__bridge RNLocalNetworkPermission *)(context);
+            [this onResult];
         }
     }
-    freeifaddrs(addrList);
-    return result;
 }
-/// Does a best effort attempt to trigger the local network privacy alert.
-///
-/// It works by sending a UDP datagram to the discard service (port 9) of every
-/// IP address associated with a broadcast-capable interface interface. This
-/// should trigger the local network privacy alert, assuming the alert hasn’t
-/// already been displayed for this app.
-///
-/// This code takes a ‘best effort’. It handles errors by ignoring them. As
-/// such, there’s guarantee that it’ll actually trigger the alert.
-///
-/// - note: iOS devices don’t actually run the discard service. I’m using it
-/// here because I need a port to send the UDP datagram to and port 9 is
-/// always going to be safe (either the discard service is running, in which
-/// case it will discard the datagram, or it’s not, in which case the TCP/IP
-/// stack will discard it).
-///
-/// There should be a proper API for this (r. 69157424).
-///
-/// For more background on this, see [Triggering the Local Network Privacy Alert](https://developer.apple.com/forums/thread/663768).
-extern void triggerLocalNetworkPrivacyAlertObjC(void) {
-    int sock4 = socket(AF_INET, SOCK_DGRAM, 0);
-    int sock6 = socket(AF_INET6, SOCK_DGRAM, 0);
-
-    if ((sock4 >= 0) && (sock6 >= 0)) {
-        char message = '!';
-        NSArray<NSData *> * addresses = addressesOfDiscardServiceOnBroadcastCapableInterfaces();
-        for (NSData * address in addresses) {
-            int sock = ((const struct sockaddr *) address.bytes)->sa_family == AF_INET ? sock4 : sock6;
-            (void) sendto(sock, &message, sizeof(message), MSG_DONTWAIT, address.bytes, (socklen_t) address.length);
-        }
-    }
-
-    // If we failed to open a socket, the descriptor will be -1 and it’s safe to
-    // close that (it’s guaranteed to fail with `EBADF`).
-    close(sock4);
-    close(sock6);
-}
-
-}
-
-- (void)requestWithResolver:(void (^ _Nonnull)(RNPermissionStatus))resolve
-                   rejecter:(void (^ _Nonnull)(NSError * _Nonnull))reject {
-  [[AVAudioSession sharedInstance] requestRecordPermission:^(__unused BOOL granted) {
-    [self checkWithResolver:resolve rejecter:reject];
-  }];
-}
-
 @end
